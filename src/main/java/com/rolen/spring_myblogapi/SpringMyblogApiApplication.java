7b44815242893f6eaba7d15642f193378f29566d
rolen <rolen@kakao.com>
2025-03-14 15:54:00 +0900

    first commit

---
+++ src/main/java/com/rolen/spring_myblogapi/SpringMyblogApiApplication.java
@@ -0,0 +1,13 @@
+package com.rolen.spring_myblogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMyblogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMyblogApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rolen/spring_myblogapi/SpringMyblogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolen.spring_myblogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMyblogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
