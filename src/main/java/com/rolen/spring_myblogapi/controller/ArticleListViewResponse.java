4acb4712f6547362bb4d0dc7aa91a2bd5bb53bfc
rolen <rolen@kakao.com>
2025-03-25 00:16:37 +0900

    [TEST] VIEW article_list

---
+++ src/main/java/com/rolen/spring_myblogapi/controller/ArticleListViewResponse.java
@@ -0,0 +1,17 @@
+package com.rolen.spring_myblogapi.controller;
+
+import com.rolen.spring_myblogapi.domain.Article;
+import lombok.Getter;
+
+@Getter
+public class ArticleListViewResponse {
+    private final long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
---
+++ src/main/java/com/rolen/spring_myblogapi/controller/BlogViewController.java
@@ -0,0 +1,25 @@
+package com.rolen.spring_myblogapi.controller;
+
+import com.rolen.spring_myblogapi.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String articles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
+}
